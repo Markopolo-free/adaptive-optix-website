4f5b7cfd8e03cd3dbbfec66efda72b987b4e3d2c
Mark Hibbard <markis@gmail.com>
2026-01-15 20:17:06 +0800

    Update config.ts from Sanity content

--- src/data/config.ts
+++ src/data/config.ts
@@ -290,6 +290,11 @@ export const config = {
     description: "Drive customer loyalty and increase transaction frequency with targeted retention strategies.",
     icon: "📊",
     name: "Repeat Business"
+  },
+  {
+    description: "Drive customer loyalty and increase transaction frequency with targeted retention strategies.",
+    icon: "📊",
+    name: "Repeat Business"
   }
 ],
   whyChooseUs: [
